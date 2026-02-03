ac675ef8674c55a1f63aa07b6213c80f5e4a28ef
SANDY <dtena90@gmail.com>
2026-02-03 17:24:43 +0100

    feat(home): update bio with strategic profile and typing effect logic

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrls: ['./home.scss']
 })
-export class Home {
+export class Home implements OnInit {
+  roles: string[] = [
+    'DESARROLLADORA FULL STACK', 
+    'DISEÑADORA GRÁFICA', 
+    'Curiosidad innata. Superarè.'
+  ];
+  
+  currentRole: string = 'S'; 
+  private roleIndex: number = 0;
+  private charIndex: number = 1;
+  private isDeleting: boolean = false;
 
+  constructor(private cdr: ChangeDetectorRef) {} 
+
+  ngOnInit(): void {
+    setTimeout(() => this.typeEffect(), 500);
+  }
+
+  typeEffect(): void {
+    const currentFullText = this.roles[this.roleIndex];
+    
+    if (this.isDeleting) {
+      this.currentRole = currentFullText.substring(0, this.charIndex - 1);
+      this.charIndex--;
+    } else {
+      this.currentRole = currentFullText.substring(0, this.charIndex + 1);
+      this.charIndex++;
+    }
+
+    this.cdr.detectChanges();
+
+    let typeSpeed = this.isDeleting ? 40 : 120;
+
+    if (!this.isDeleting && this.charIndex === currentFullText.length) {
+      typeSpeed = 2000; 
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.charIndex === 0) {
+      this.isDeleting = false;
+      this.roleIndex = (this.roleIndex + 1) % this.roles.length;
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.typeEffect(), typeSpeed);
+  }
 }
